ca5df07f38f07f1a6e840acaa17295828c126446
Unknown <david.vanhoecke@delair-tech.com>
2017-07-04 17:13:03 +0200

    Continued model

--- Arduino.VoltageSensing/sketch_jul03a/sketch_jul03a.ino
+++ Arduino.VoltageSensing/sketch_jul03a/sketch_jul03a.ino
@@ -1,21 +1,30 @@
-long readVcc() {
-  long result;
-  // Read 1.1V reference against AVcc
-  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA,ADSC));
-  result = ADCL;
-  result |= ADCH<<8;
-  result = 1126400L / result; // Back-calculate AVcc in mV
-  return result;
-}
+#include <EmonLib.h>
 
-void setup() {
+
+EnergyMonitor emon1;
+double Irms;
+double Vrms;
+double Pwr;
+static char vrms_char[10];
+static char irms_char[10];
+static char pwr_char[10];
+
+void setup() 
+{
   Serial.begin(9600);
+  emon1.voltage(1.1, 370.0, 1.72);  // Voltage: input pin, calibration, phase_shift
+  emon1.current(0, 10);           // Current: input pin, calibration.
 }
 
-void loop() {
-  Serial.println( readVcc(), DEC );
-  delay(1000);
+static word calculate()
+{
+  emon1.calcVI(6, 1000); // Calculate all. No.of half wavelengths (crossings), time-out
+  Vrms = emon1.Vrms;
+  
+  Serial.println(Vrms);
+}
+
+void loop()
+{
+  calculate();
 }
