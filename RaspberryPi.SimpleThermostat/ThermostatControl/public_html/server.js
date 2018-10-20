0df26fbe89bea20f3c483f4df0af906d144414f9
Unknown <david.vanhoecke@delair-tech.com>
2018-10-21 01:12:25 +0200

    Added feature to read current relay state and updated UI accordingly

--- RaspberryPi.SimpleThermostat/ThermostatControl/public_html/server.js
+++ RaspberryPi.SimpleThermostat/ThermostatControl/public_html/server.js
@@ -1,25 +1,33 @@
 var http = require('http').createServer(handler); //require http server, and create server with function handler()
 var fs = require('fs'); //require filesystem module
 var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+var url = require('url');
 var gpio = require('rpi-gpio');
 var sensor = require('node-dht-sensor');
 var relayPin = 23;
 var dht11Pin = 25;
 var started = false;
+var heaterIsOn = false;
+var accesKey = "59997d31-3bd6-4565-b564-362920cf644e";
+
 gpio.setMode(gpio.MODE_BCM);
 gpio.setup(relayPin, gpio.DIR_OUT, logSetup);
 http.listen(8080); //listen to port 8080
 
+
 function handler (req, res) { //create server
-  fs.readFile('index.html', function(err, data) { //read file index.html in public folder
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
-    res.write(data); //write data from index.html
-    return res.end();
-  });
+  srvUrl = url.parse(`http://${req.url}`);
+  if(srvUrl.path.indexOf(accesKey) > -1){
+    fs.readFile('/home/pi/scripts/server/index.html', function(err, data) { //read file index.html in public folder
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'}); //display 404 on error
+        return res.end("404 Not Found");
+      } 
+      res.writeHead(200, {'Content-Type': 'text/html'}); //write HTML
+      res.write(data); //write data from index.html
+      return res.end();
+    });
+  }
 }
 
 
@@ -28,25 +36,34 @@ function handler (req, res) { //create server
 io.sockets.on('connection', function (socket) {// WebSocket Connection
   socket.on('cmd', function(data) { //get light switch status from client
     console.log("cmd data received: " + data); 
-    readSensor(socket);
+    
     //startReadings(socket);
     if (data === "on") {
         write(true);
-        
     }
     
     if (data === "off") {
         write(false);
     }
+    
+    readSensor(socket);
   });
   
-  socket.emit("status", "initialized");
+  socket.emit("status", "Connected");
 });
 
 function write(value) {
     gpio.write(relayPin, value, function(err) {
         if (err) throw err;
-        console.log('Written to pin');
+        console.log('Written to pin: ' + value);
+    });
+}
+
+function readRelayState(){
+    gpio.read(relayPin, function(readError, bHeaterState) {
+        if (readError) throw readError;
+        //console.log('The value is ' + heaterState);
+        heaterIsOn = bHeaterState;
     });
 }
 
@@ -66,12 +83,13 @@ function startReadings(socket){
 }
 
 function readSensor(socket){
+    readRelayState();   
     sensor.read(11, dht11Pin, function(err, temperature, humidity) {
         if (!err) {
             console.log('date: ' + getTheDate() + ', temp: ' + temperature.toFixed(0) + '°C, ' +
-                'humidity: ' + humidity.toFixed(0) + '%'
+                'humidity: ' + humidity.toFixed(0) + '%' + ", heater is on: " + heaterIsOn
             );
-            socket.emit("conditions", {t:temperature.toFixed(0),h:humidity.toFixed(0),d:getTheDate()});
+            socket.emit("conditions", {t:temperature.toFixed(0),h:humidity.toFixed(0),d:getTheDate(),s:heaterIsOn});
         }
     });
 }
